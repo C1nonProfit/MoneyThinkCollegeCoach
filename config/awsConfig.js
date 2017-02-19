b457d8591439c862e95ca72464787eba31138111
Shravan Reddy <tshravankumarreddy@gmail.com>
2017-02-19 09:14:50 -0600

    Update awsConfig.js

--- config/awsConfig.js
+++ config/awsConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   region: "us-east-1",
   endpoint: "http://dynamodb.us-east-1.amazonaws.com",
-  "accessKeyId": "AKIAJZ5Q6JVPOP6AILBA",
-  "secretAccessKey": "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"
+  "accessKeyId": "someAccessKeyId",
+  "secretAccessKey": "SomeSecretKey"
 }
