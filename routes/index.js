e48a37b2c710c9a4d6e1c4fd67fb45fb455b6ec2
Shravan Reddy <ujl168@capitalone.com>
2017-02-18 18:09:17 -0600

    second

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const students = require('./students');
+
+routes.use('/students', students);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('dashboard', { title: 'College Coach' });
 });
+
+router.get('/students', function(req, res, next) {
+  res.render('dashboard', { title: 'College Coach' });
+});
+
+
 
 module.exports = router;
